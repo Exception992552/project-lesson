29d0eddde65c26b2ecd19810bb28b1875d6277e7
Sergey <example@gmail.com>
2023-02-25 18:02:46 +0200

    Practice 1: create personaMovieDB object

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
-(function () {
+ "use strict";
 
-    "use strict";
+ let nymberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
+ const personaMovieDB = {
+     count: nymberOfFilms,
+     movies: {},
+     actors: {
 
+     },
+     genres: [],
+     privat: false
 
-
-
-}());
+ };
+ for (let i = 0; i < 2; i++) {
+     let lastSeenFilm = prompt('Один из последних просмотренных фильмов', '');
+     let scoreLastSeenFilm = +prompt('На сколько оцените его ?', '');
+     personaMovieDB.movies[lastSeenFilm] = scoreLastSeenFilm;
+ }
